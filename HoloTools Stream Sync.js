eb70d4ac9e1c1176203d6d9a39e814e67206f80e
XingYan <46369463+XingYanTW@users.noreply.github.com>
2021-09-05 21:24:25 +0800

    Add files via upload

---
+++ HoloTools Stream Sync.js
@@ -0,0 +1,16 @@
+// ==UserScript==
+// @name         HoloTools Stream Sync
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  try to take over the world!
+// @author       You
+// @match        https://hololive.jetri.co/*
+// @icon         https://www.google.com/s2/favicons?domain=youtube.com
+// @grant        none
+// ==/UserScript==
+
+(function() {
+'use strict';
+localStorage.setItem('ruleOnePlayer', 0);
+localStorage.setItem('rulePauseOther', 0);
+})();
